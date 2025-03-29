ceb126a3f830c93549ab9726c94031fa3fcd9d29
deveshcse <d.mishra56603@gmail.com>
2025-03-29 19:14:44 +0530

    Add initial implementation of addition functionality and basic HTML structure

---
+++ L1/app.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+var n1 = 20;
+var n2 = 30;
+var ans = add(n1, n2);
+console.log(ans);
---
+++ L1/app.ts
@@ -0,0 +1,10 @@
+function add(num1: number, num2: number){
+    return num1+num2;
+}
+
+const n1 = 20
+const n2 = 30
+const printResule = true;
+const ans = add (n1, n2);
+
+console.log(ans); 
